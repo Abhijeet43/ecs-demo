5ae9d4557022103d7a228f44c79feab921d0c332
abhijeet <abhijeetsinha43@gmail.com>
2024-05-02 20:54:40 +0530

    ecs demo code

---
+++ server.js
@@ -0,0 +1,13 @@
+import app from './app.js';
+
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Congratulations we are in ECS demo');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('ECS demo is healthy');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
